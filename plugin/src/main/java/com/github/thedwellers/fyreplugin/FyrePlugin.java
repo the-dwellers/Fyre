1f544ee8489ceb6874f0fe23f1c36a254a84e8c6
brandagot <brandagot@hotmail.co.uk>
2019-07-24 14:50:08 +0100

    Added FlyrPlugin back and started Merchant commands

---
+++ plugin/src/main/java/com/github/thedwellers/fyreplugin/FyrePlugin.java
@@ -0,0 +1,16 @@
+package com.github.thedwellers.fyreplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import com.github.thedwellers.fyreplugin.commands.MerchantCommand;
+
+public final class FyrePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+}
---
+++ plugin/src/main/java/com/github/thedwellers/fyreplugin/commands/MerchantCommand.java
@@ -0,0 +1,11 @@
+package com.github.thedwellers.fyreplugin.commands;
+import org.bukkit.command.*;
+import org.bukkit.inventory.Merchant;
+
+public class MerchantCommand implements CommandExecutor{
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
+
