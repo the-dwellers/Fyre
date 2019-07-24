3ee63cda1b7d43a2d2f9b0ecf9ca3c175ce541c4
WYVERN <35181365+WYVERN2742@users.noreply.github.com>
2019-07-25 00:22:10 +0100

    Use Abstract Command

--- plugin/src/main/java/com/github/thedwellers/fyreplugin/FyrePlugin.java
+++ plugin/src/main/java/com/github/thedwellers/fyreplugin/FyrePlugin.java
@@ -2,12 +2,14 @@ package com.github.thedwellers.fyreplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import com.github.thedwellers.fyreplugin.commands.MerchantCommand;
+import com.github.thedwellers.fyreplugin.commands.StatusCommand;
 
 public final class FyrePlugin extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
 		this.getCommand("merchant").setExecutor(new MerchantCommand());
+		this.getCommand("status").setExecutor(new StatusCommand(this));
 	}
 
 	@Override
