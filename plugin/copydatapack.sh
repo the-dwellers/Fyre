b7f9da57ea62c6005c0aaf6e67096fefc51f23d7
BuildTools <brandagot@hotmail.co.uk>
2019-07-24 21:55:15 +0100

    Fixed copydatapack.sh so directory exists

--- plugin/copydatapack.sh
+++ plugin/copydatapack.sh
@@ -3,4 +3,4 @@
 # Copies the datapacks from Fyre/Datapack to server/world/datapacks
 # Remember to execute minecraft:reload in the server console after copying. The server does not need to be restarted.
 
-cp -r "../datapack/." "./server/world/datapacks/."
+mkdir -p "./server/world/datapacks" && cp -r "../datapack/." "./server/world/datapacks/."
