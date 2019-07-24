8db50bc2d9e8ecbda14679d2445ab5e8fb3ea6f8
WYVERN <35181365+WYVERN2742@users.noreply.github.com>
2019-07-24 19:34:41 +0100

    Remove out-of-date comment

--- plugin/copydatapack.bat
+++ plugin/copydatapack.bat
@@ -1,6 +1,5 @@
 @echo off
 REM Copies the datapacks from Fyre/Datapack to server/world/datapacks
 REM Remember to execute minecraft:reload in the server console after copying. The server does not need to be restarted.
-REM Note: if newserver.bat is ran, the datapack will need to be copied again
 
 xcopy "..\datapack" ".\server\world\datapacks" /S /E /D /Q
