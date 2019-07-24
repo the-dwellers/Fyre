acb0a26a436b0142b8e66ecb31dbd9c1c9cc343b
Brandagot <brandagot@hotmail.co.uk>
2019-07-24 23:28:20 +0100

    Added player join listener

--- plugin/src/main/java/com/github/thedwellers/fyreplugin/FyrePlugin.java
+++ plugin/src/main/java/com/github/thedwellers/fyreplugin/FyrePlugin.java
@@ -2,16 +2,26 @@ package com.github.thedwellers.fyreplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import com.github.thedwellers.fyreplugin.commands.MerchantCommand;
+import com.github.thedwellers.fyreplugin.events.PlayerJoin;
 
 public final class FyrePlugin extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-		this.getCommand("merchant").setExecutor(new MerchantCommand());
+		registerCommands();
+		registerListeners();
 	}
 
 	@Override
 	public void onDisable() {
+	}
+
+	private void registerCommands() {
+		this.getCommand("merchant").setExecutor(new MerchantCommand());
+	}
+
+	private void registerListeners() {
+		getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
 	}
 
 }
--- plugin/src/main/java/com/github/thedwellers/fyreplugin/events/PlayerJoin.java
+++ plugin/src/main/java/com/github/thedwellers/fyreplugin/events/PlayerJoin.java
@@ -1,9 +1,17 @@
 package com.github.thedwellers.fyreplugin.events;
 
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 
 public class PlayerJoin implements Listener{
-	
+	@EventHandler()
+	public void onPlayerJoin(PlayerJoinEvent event){
+		Player player = event.getPlayer();
+		
+	}
 }
 
 
