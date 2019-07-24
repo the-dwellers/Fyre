bc7592a513c808db3c1d81b01b12cfb5fbc52cdc
WYVERN <35181365+WYVERN2742@users.noreply.github.com>
2019-07-24 16:47:13 +0100

    Run Formatter

--- plugin/src/main/java/com/github/thedwellers/fyreplugin/commands/MerchantCommand.java
+++ plugin/src/main/java/com/github/thedwellers/fyreplugin/commands/MerchantCommand.java
@@ -1,16 +1,15 @@
 package com.github.thedwellers.fyreplugin.commands;
+
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Merchant;
 
-public class MerchantCommand implements CommandExecutor{
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if(sender instanceof Player){
+public class MerchantCommand implements CommandExecutor {
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (sender instanceof Player) {
 			sender.sendMessage("Hello there");
 			return true;
 		}
-        return false;
-    }
+		return false;
+	}
 }
-
