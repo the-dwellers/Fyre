3e6e2f030af8d6cd82b29ee00d1e9e92af53859d
WYVERN <35181365+WYVERN2742@users.noreply.github.com>
2019-07-26 23:53:15 +0100

    Add TPS to Status Command

--- plugin/src/main/java/com/github/thedwellers/fyreplugin/commands/StatusCommand.java
+++ plugin/src/main/java/com/github/thedwellers/fyreplugin/commands/StatusCommand.java
@@ -1,11 +1,14 @@
 package com.github.thedwellers.fyreplugin.commands;
 
+import java.text.DecimalFormat;
+
 import com.github.thedwellers.fyreplugin.configuration.Strings;
 
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import net.md_5.bungee.api.ChatColor;
@@ -84,8 +87,8 @@ public class StatusCommand extends AbstractCommand {
 			loadedChunks += world.getLoadedChunks().length;
 		}
 
-		TextComponent chunkText = new TextComponent("\n"+
-				Strings.OUT_PREFIX + "Loaded Chunks: " + Strings.C_ACCENT + loadedChunks);
+		TextComponent chunkText = new TextComponent(
+				"\n" + Strings.OUT_PREFIX + "Loaded Chunks: " + Strings.C_ACCENT + loadedChunks);
 
 		if (isPlayer) {
 			// Add hover for player
@@ -96,7 +99,8 @@ public class StatusCommand extends AbstractCommand {
 		} else {
 			// Caller is console
 			if (players.length != 0) {
-				chunkText.addExtra("\n"+Strings.OUT_PREFIX + "Chunks per player: " + Strings.C_ACCENT + loadedChunks / players.length);
+				chunkText.addExtra("\n" + Strings.OUT_PREFIX + "Chunks per player: " + Strings.C_ACCENT
+						+ loadedChunks / players.length);
 			}
 		}
 		responseText.addExtra(chunkText);
@@ -106,6 +110,62 @@ public class StatusCommand extends AbstractCommand {
 		responseText.addExtra(PluginCommand.getPlugins(sender));
 
 		// TPS
+		ChatColor[] tpsColors = new ChatColor[4];
+
+		// tps[] = [1m, 5m, 15m, average]
+		double[] tps = new double[4];
+		tps[0] = plugin.getServer().getTPS()[0];
+		tps[1] = plugin.getServer().getTPS()[1];
+		tps[2] = plugin.getServer().getTPS()[2];
+		tps[3] = (tps[0] + tps[1] + tps[2]) / 3;
+
+		for (int i = 0; i < tps.length; i++) {
+			if (tps[i] >= 20.0) {
+				tpsColors[i] = ChatColor.DARK_GREEN;
+			} else if (tps[i] > 19.95) {
+				tpsColors[i] = ChatColor.GREEN;
+			} else if (tps[i] > 18.5) {
+				tpsColors[i] = ChatColor.YELLOW;
+			} else if (tps[i] > 17.0) {
+				tpsColors[i] = ChatColor.RED;
+			} else {
+				tpsColors[i] = ChatColor.DARK_RED;
+			}
+		}
+
+		DecimalFormat decimalFormat = new DecimalFormat("0.00");
+		TextComponent tpsText = new TextComponent(Strings.OUT_PREFIX + "Ticks Per Second: " + tpsColors[0] + decimalFormat.format(tps[0]));
+
+		if (isPlayer) {
+			TextComponent hoverText = new TextComponent(
+					"" + tpsColors[0] + decimalFormat.format(tps[0]) +
+					" " + tpsColors[1] + decimalFormat.format(tps[1]) +
+					" " + tpsColors[2] + decimalFormat.format(tps[2]) + "\n");
+			switch (tpsColors[3]) {
+			case DARK_GREEN:
+				hoverText.addExtra(tpsColors[3] + "TPS is perfect");
+				break;
+			case GREEN:
+				hoverText.addExtra(tpsColors[3] + "Unnoticeable TPS loss");
+				break;
+			case YELLOW:
+				hoverText.addExtra(tpsColors[3] + "Some Minor Problems");
+				break;
+			case RED:
+				hoverText.addExtra(tpsColors[3] + "Major Server Problems");
+				break;
+			default:
+				hoverText.addExtra(tpsColors[3] + "Unplayable");
+				break;
+			}
+			tpsText.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponent[] { hoverText }));
+		} else {
+			tpsText.addExtra(" " + tpsColors[1] + decimalFormat.format(tps[1])
+			+ " " + tpsColors[2] + decimalFormat.format(tps[2]));
+		}
+		responseText.addExtra("\n");
+		responseText.addExtra(tpsText);
+
 		// TODO: Show Server Ticks Per Second
 
 		// RAM
