102b887a7e4d3b912058e17abcfbf57cea4eed1b
Peetu Saarinen <chargedbyte@gmail.com>
2019-07-28 06:39:23 +0300

    Clean up the dev_server.py for better readability

--- plugin/dev_server.py
+++ plugin/dev_server.py
@@ -5,10 +5,17 @@ import requests
 import hashlib
 import shutil
 
-if platform.system().lower() == "windows":
-	subprocess.call("cls")
-else:
-	subprocess.call("clear")
+### Functions ###
+
+def clear_console():
+	if platform.system().lower() == "windows":
+		subprocess.call("cls")
+	else:
+		subprocess.call("clear")
+
+### Main Script ###
+
+clear_console()
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
@@ -40,6 +47,7 @@ if not os.path.exists("eula.txt"):
 	f = open("eula.txt", "w+")
 	f.write("eula=true")
 	f.close()
+
 	print("Minecraft EULA accepted")
 
 if not os.path.exists("world"):
@@ -51,15 +59,19 @@ if os.path.exists("world/datapack"):
 shutil.copytree("../../datapack/", "world/datapack")
 
 headers = {"user-agent": "fyre-devserver-script/1.0.0"}
+
 if not os.path.exists("server.jar"):
 	print("Downloading PaperMC...")
+
 	r = requests.get("https://papermc.io/api/v1/paper/1.14.4/latest/download", headers=headers)
 	f = open("server.jar", "wb")
 	f.write(r.content)
 	f.close()
+
 	print("PaperMC downloaded")
 else:
 	print("Checking for PaperMC updates...")
+
 	r = requests.get("https://papermc.io/api/v1/paper/1.14.4/latest/download", headers=headers)
 
 	h1 = hashlib.sha256()
@@ -68,17 +80,20 @@ else:
 	f = open("server.jar", "rb")
 	h1.update(f.read())
 	f.close()
-
 	h2.update(r.content)
 
 	if str(h1.hexdigest()) != str(h2.hexdigest()):
 		print("Update found, installing the latest version now")
+
 		if os.path.exists("server.jar.old"):
 			os.remove("server.jar.old")
+
 		os.rename("server.jar", "server.jar.old")
+
 		f = open("server.jar", "wb")
 		f.write(r.content)
 		f.close()
+
 	else:
 		print("No updates found, continuing...")
 
@@ -86,9 +101,6 @@ else:
 
 print("Starting the server now")
 
-if platform.system().lower() == "windows":
-	subprocess.call("cls")
-else:
-	subprocess.call("clear")
+clear_console()
 
 subprocess.call(["java", "-Xms256M", "-Xmx1024M", "-jar", "server.jar", "nogui", "-h", "127.0.0.1"])
