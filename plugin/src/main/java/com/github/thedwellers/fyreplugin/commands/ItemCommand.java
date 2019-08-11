e10cfa213e561754a641eb59e09dbbc9f3b27176
WYVERN <35181365+WYVERN2742@users.noreply.github.com>
2019-08-11 17:14:26 +0100

    Display custom item names and total item amount

--- plugin/src/main/java/com/github/thedwellers/fyreplugin/commands/ItemCommand.java
+++ plugin/src/main/java/com/github/thedwellers/fyreplugin/commands/ItemCommand.java
@@ -3,6 +3,7 @@ package com.github.thedwellers.fyreplugin.commands;
 import com.github.thedwellers.fyreplugin.ChatManager;
 import com.github.thedwellers.fyreplugin.Reflected;
 import com.github.thedwellers.fyreplugin.exceptions.ReflectionFailedException;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -28,13 +29,38 @@ public class ItemCommand implements CommandExecutor {
 			try {
 				String nbt = Reflected.stackToJson(item);
 				TextComponent text;
+				int amount = 0;
 
-				if (item.getAmount() == 1) {
-					text = new TextComponent(
-							ChatColor.GREEN + "" + ChatColor.BOLD + "[" + item.getI18NDisplayName() + "]");
+				// Oh look, multiple methods to get player inventory contents...
+				for (ItemStack stack : player.getInventory().getContents()) {
+					if (stack!=null && stack.isSimilar(item)) {
+						amount += stack.getAmount();
+					}
+				}
+
+				String itemName = item.getItemMeta().hasDisplayName() ? item.getItemMeta().getDisplayName() : item.getI18NDisplayName();
+
+				// Check to see if the item has a custom colored name. this includes enchanted and renamed items
+				// * Due to java optimization, we can bundle in the display name check using logical and for null checks
+				boolean customItem = itemName.toCharArray()[0] == '\u00a7' && itemName.toCharArray()[1] != 'o';
+
+				// Default to a light green color
+				String color = "\u00a7a";
+
+				if (customItem) {
+					// Item has a custom name / color
+					color = String.valueOf(new char[]{'\u00a7',itemName.toCharArray()[1]});
 				} else {
-					text = new TextComponent(ChatColor.GREEN + "" + ChatColor.BOLD + "[" + item.getAmount() + "x "
-							+ item.getI18NDisplayName() + "]");
+					// If the item is not a custom item, then display the default name to chat, rather than the rename
+					itemName = item.getI18NDisplayName();
+				}
+
+				if (amount == 1) {
+					text = new TextComponent(
+							color + ChatColor.BOLD + "[" + itemName + "]");
+				} else {
+					text = new TextComponent(color + ChatColor.BOLD + "[" + amount + "x "
+							+ itemName + "]");
 				}
 
 				text.setHoverEvent(new HoverEvent(Action.SHOW_ITEM, new TextComponent[] { new TextComponent(nbt) }));
