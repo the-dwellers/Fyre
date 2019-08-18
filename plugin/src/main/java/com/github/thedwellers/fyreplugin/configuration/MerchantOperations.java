b458e9fa12df9f7f7195b606210e33faa988c5b7
Brandagot <brandagot@hotmail.co.uk>
2019-08-18 21:10:59 +0100

    method for returning tiers for merchants

--- plugin/src/main/java/com/github/thedwellers/fyreplugin/configuration/MerchantOperations.java
+++ plugin/src/main/java/com/github/thedwellers/fyreplugin/configuration/MerchantOperations.java
@@ -1,14 +1,16 @@
 package com.github.thedwellers.fyreplugin.configuration;
 
 import org.bukkit.entity.Villager;
+import org.bukkit.inventory.MerchantRecipe;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import com.github.thedwellers.fyreplugin.configuration.MerchantRecipeList;
 
 public class MerchantOperations {
-	public static void setFarmerRecipies(Villager villager, String tier){
-		switch(tier) {
-			case "0":
-				villager.setRecipes(MerchantRecipeList.farmerTier0());
-				break;
-		}
+	public static void setFarmerRecipes(Villager villager, int tier){
+
+
 	}
 }
