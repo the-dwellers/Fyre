0fc1bcb243b8978191672b07fd4b9371911ab046
WYVERN2742 <35181365+WYVERN2742@users.noreply.github.com>
2019-11-17 15:06:35 +0000

    Move entity health increase to Attributes class

--- plugin/src/main/java/com/github/thedwellers/fyreplugin/Attributes.java
+++ plugin/src/main/java/com/github/thedwellers/fyreplugin/Attributes.java
@@ -1,6 +1,10 @@
 package com.github.thedwellers.fyreplugin;
 
+import org.bukkit.attribute.Attributable;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.attribute.AttributeModifier.Operation;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 /**
@@ -10,11 +14,42 @@ public abstract class Attributes {
 
 	/**
 	 * Update player attributes.
+	 *
 	 * @param player Player to update attributes of
 	 */
 	public static void applyPlayer(Player player) {
 		// 5 hearts of health
 		player.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(10);
+	}
+
+	public static void applyMob(Entity entity) {
+		if (entity instanceof Attributable) {
+			switch (entity.getType()) {
+			case ZOMBIE:
+			case SKELETON:
+			case SPIDER:
+			case ENDERMAN:
+				applyOverworldHostile((Attributable) entity);
+				break;
+			case BLAZE:
+			case GHAST:
+			case WITHER_SKELETON:
+				applyNetherHostile((Attributable) entity);
+			default:
+				break;
+			}
+		}
+	}
+
+	public static void applyOverworldHostile(Attributable entity) {
+		entity.getAttribute(Attribute.GENERIC_MAX_HEALTH)
+				.addModifier(new AttributeModifier("Fyre Spawn Health 1.5", 0.5, Operation.MULTIPLY_SCALAR_1));
+	}
+
+	public static void applyNetherHostile(Attributable entity) {
+		entity.getAttribute(Attribute.GENERIC_MAX_HEALTH)
+				.addModifier(new AttributeModifier("Fyre Spawn Health 2", 1, Operation.MULTIPLY_SCALAR_1));
+
 	}
 
 }
--- plugin/src/main/java/com/github/thedwellers/fyreplugin/events/EntitySpawn.java
+++ plugin/src/main/java/com/github/thedwellers/fyreplugin/events/EntitySpawn.java
@@ -1,10 +1,8 @@
 package com.github.thedwellers.fyreplugin.events;
 
-import org.bukkit.attribute.Attributable;
-import org.bukkit.attribute.Attribute;
-import org.bukkit.attribute.AttributeModifier;
-import org.bukkit.attribute.AttributeModifier.Operation;
-import org.bukkit.entity.LivingEntity;
+import com.github.thedwellers.fyreplugin.Attributes;
+
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
@@ -16,30 +14,9 @@ public class EntitySpawn implements Listener {
 
 	@EventHandler()
 	private void onEntitySpawn(EntitySpawnEvent event) {
-		if (event.getEntity() instanceof LivingEntity) {
-			LivingEntity livingEntity = (LivingEntity) event.getEntity();
-			if (livingEntity instanceof Attributable) {
-				Attributable attrib = (Attributable) livingEntity;
-				AttributeModifier mod1 = new AttributeModifier("Fyre Spawn Health 1.5", 0.5, Operation.MULTIPLY_SCALAR_1);
-				AttributeModifier mod2 = new AttributeModifier("Fyre Spawn Health 2", 1, Operation.MULTIPLY_SCALAR_1);
-				switch (livingEntity.getType()) {
-				case ZOMBIE:
-				case SKELETON:
-				case SPIDER:
-				case ENDERMAN:
-				attrib.getAttribute(Attribute.GENERIC_MAX_HEALTH).addModifier(mod1);
-				break;
-				case BLAZE:
-				case GHAST:
-				case WITHER_SKELETON:
-					attrib.getAttribute(Attribute.GENERIC_MAX_HEALTH).addModifier(mod2);
-				default:
-					break;
-				}
-
-			}
+		if (!(event.getEntity() instanceof Player)) {
+			Attributes.applyMob(event.getEntity());
 		}
-
 	}
 
 }
