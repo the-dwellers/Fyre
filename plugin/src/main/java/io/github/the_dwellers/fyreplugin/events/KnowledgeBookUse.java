dd054ce1f4758e914f1038fd3701294dde2eb150
WYVERN2742 <35181365+WYVERN2742@users.noreply.github.com>
2019-12-30 02:53:27 +0000

    Fix tome stacktrace

--- plugin/src/main/java/io/github/the_dwellers/fyreplugin/events/KnowledgeBookUse.java
+++ plugin/src/main/java/io/github/the_dwellers/fyreplugin/events/KnowledgeBookUse.java
@@ -3,6 +3,7 @@ package io.github.the_dwellers.fyreplugin.events;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.SoundCategory;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -14,9 +15,10 @@ public class KnowledgeBookUse implements Listener {
 
 	@EventHandler()
 	public void onKnowledgeBook(PlayerInteractEvent event) {
-		if (event.getItem().getType() != Material.KNOWLEDGE_BOOK) {
-			return;
+		Player player = event.getPlayer();
+		if (player.getInventory().getItemInMainHand().getType() == Material.KNOWLEDGE_BOOK) {
+			event.getPlayer().playSound(event.getPlayer().getLocation(), Sound.ENTITY_ENDER_EYE_DEATH,
+					SoundCategory.MASTER, 1, 1);
 		}
-		event.getPlayer().playSound(event.getPlayer().getLocation(), Sound.ENTITY_ENDER_EYE_DEATH, SoundCategory.MASTER, 1, 1);
 	}
 }
