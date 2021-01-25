113e332a6b20d5cd65f3accac972addcf3283bfd
WYVERN2742 <35181365+WYVERN2742@users.noreply.github.com>
2021-01-25 17:57:49 +0000

    Use damage event for cactus damage.

--- plugin/src/main/java/io/github/the_dwellers/fyreplugin/features/SpikyCactus.java
+++ plugin/src/main/java/io/github/the_dwellers/fyreplugin/features/SpikyCactus.java
@@ -4,6 +4,8 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.entity.EntityDamageByBlockEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
 import io.github.the_dwellers.fyreplugin.configuration.SupportedVersions;
 import io.github.the_dwellers.fyreplugin.core.AbstractFeature;
@@ -35,7 +37,18 @@ public class SpikyCactus extends AbstractFeature implements Listener {
 		if (event.getBlock().getType() == Material.CACTUS &&
 				event.getPlayer().getEquipment().getItemInMainHand().getType() == Material.AIR) {
 			// Empty hand punching a cactus
-			event.getPlayer().damage(0.5);
+
+			// Create a damage event and call it to apply any changes other events may do.
+			EntityDamageByBlockEvent prickEvent = new EntityDamageByBlockEvent(event.getBlock(), event.getPlayer(), DamageCause.CONTACT, 0.5);
+			boolean shouldDamagePlayer = prickEvent.callEvent();
+
+			// Check to see if the event was cancelled by an event.
+			if (shouldDamagePlayer) {
+				// Set the damage cause, and damage player
+				event.getPlayer().setLastDamageCause(prickEvent);
+				event.getPlayer().damage(prickEvent.getFinalDamage());
+			}
+
 		}
 	}
 }
